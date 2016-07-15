9c42f969713a3381422ede12669c498b2d0f424c
Jesus Luna Huerta <jmlunahuerta@gmail.com>
2016-07-15 13:50:39 +0200

    second commit

---
+++ app/scripts/controllers/map.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name proyectoFinalApp.controller:MapCtrl
+ * @description
+ * # MapCtrl
+ * Controller of the proyectoFinalApp
+ */
+angular.module('proyectoFinalApp')
+  .controller('MapCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
